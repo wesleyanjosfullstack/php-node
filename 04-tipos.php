64ecf5bb42421173508799d170be29cd234453c0
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-04-02 07:50:22 -0300

    validando os tipos de dados

--- 04-tipos.php
+++ 04-tipos.php
@@ -4,5 +4,25 @@
 // string
 $name = 'Wesley Anjos';
 var_dump($name);
+echo '<br>';
+
+if (is_string($name)) {
+    echo 'É uma string!<br>';
+} else {
+    echo 'Não é uma string<br>';
+}
+
+echo '<hr>';
+
+// int
+$age = 28;
+var_dump($age);
+echo '<br>';
+
+if (is_int($age)) {
+    echo 'É uma int!<br>';
+} else {
+    echo 'Não é uma int<br>';
+}
 
 ?>
