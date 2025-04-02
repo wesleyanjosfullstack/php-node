388ff85fad4e279adc238f0b6dbd9652bb211aa2
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-04-02 07:28:51 -0300

    trabalhando com as variáveis

---
+++ 02-variaveis.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = 'Wesley Anjos';
+$age = 28;
+$height = 1.7;
+
+echo 'Meu nome é '. $name .', minha idade '. $age .' e minha altura é '. $height .'m !<br>';
+
+$name = 'Felipe Cabral';
+$age = 29;
+$height = 1.90;
+
+echo 'Meu nome é '. $name .', minha idade '. $age .' e minha altura é '. $height .'m !<br>';
+?>
