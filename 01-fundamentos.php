fde1cd1666e90cab9320911a93ed7b8baeaead29
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-04-02 07:22:20 -0300

    adicionando arquivo php

---
+++ 01-fundamentos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Nodestudio</title>
+</head>
+<body>
+    <?php
+        $n = 'Wesley Anjos';
+        $a = 28;
+        echo '<p>Meu nome é '. $n .' e minha idade é '. $a .' !</p>';
+    ?>
+</body>
+</html>
